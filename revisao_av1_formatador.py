8c62b67ccb3e0aa80af123acb5d01f6d55767a54
a <a>
2026-08-25 09:42:14 -0300

    revisao av1

---
+++ revisao_av1_formatador.py
@@ -0,0 +1,30 @@
+# ==============================================================================
+# REVISAO PARA A AV1: MODULARIZACAO E MANIPULACAO DE STRINGS
+# ARQUIVO: revisao_av1_formatador.py
+# Nome do Aluno:
+# Data:
+# ==============================================================================
+
+def formatar_citacao(nome_completo):
+    partes = nome_completo.split()
+
+    sobrenome = partes[-1].upper()
+
+    nome = " ".join(partes[:-1])
+
+    return sobrenome + ", " + nome
+
+
+def gerar_codigo(ano, cpf):
+    cpf = cpf.strip()
+
+    tres_primeiros_digitos = cpf[:3]
+
+    return "ALU-" + ano + "-" + tres_primeiros_digitos
+
+autor = "Carlos Eduardo Andrade"
+citacao_formatada = formatar_citacao(autor)
+print("Citacao Bibliografica:", citacao_formatada)
+
+matricula = gerar_codigo("2026", "456.789.123-00")
+print("Matricula Gerada     :", matricula)
